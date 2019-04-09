46a234e55dac1eb9182f584d3992f356573f4898
LysleB <falalayl@gmail.com>
2019-04-09 12:57:04 +0800

    Updated DB

--- config/sbacms.sql
+++ config/sbacms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2019 at 03:28 PM
+-- Generation Time: Apr 09, 2019 at 06:56 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -29,72 +29,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `appointment` (
-  `appId` int(11) NOT NULL,
-  `custId` int(11) DEFAULT NULL,
-  `employeeId` int(11) DEFAULT NULL,
-  `serviceId` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `customer` varchar(50) DEFAULT NULL COMMENT 'customer''s username',
+  `employee` varchar(50) DEFAULT NULL COMMENT 'employee''s username',
+  `service_id` int(11) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `time` time DEFAULT NULL,
-  `serviceName` varchar(20) DEFAULT NULL,
-  `price` int(6) DEFAULT NULL,
-  `status` int(2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
-
-CREATE TABLE `customer` (
-  `custId` int(11) NOT NULL,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
-  `userName` varchar(20) NOT NULL,
-  `address` varchar(90) NOT NULL,
-  `contactNo` int(13) NOT NULL,
-  `emailAdd` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `status` varchar(25) DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `customer`
+-- Dumping data for table `appointment`
 --
 
-INSERT INTO `customer` (`custId`, `firstName`, `lastName`, `userName`, `address`, `contactNo`, `emailAdd`, `password`) VALUES
-(3, 'Ken', 'Balas', 'Kendall', '', 0, 'k@gmail.com', 'ken123'),
-(4, 'John', 'Derick', 'Jaydee', '', 0, 'jd@gmail.com', 'jd123'),
-(5, 'sharmaine', 'rufino', 'sharmaine', '', 0, 'sharmainerufino@gmai', 'sharmaine123'),
-(6, 'john', 'de vera', 'Jddevera', '', 0, 'sampleemail@yahoo.co', 'bliss123');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employee`
---
-
-CREATE TABLE `employee` (
-  `employeeId` int(11) NOT NULL,
-  `firstName` varchar(30) NOT NULL,
-  `lastName` varchar(30) NOT NULL,
-  `userName` varchar(18) NOT NULL,
-  `address` varchar(90) NOT NULL,
-  `contactNo` int(13) NOT NULL,
-  `emailAdd` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `type` varchar(10) NOT NULL,
-  `accLvl` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `employee`
---
-
-INSERT INTO `employee` (`employeeId`, `firstName`, `lastName`, `userName`, `address`, `contactNo`, `emailAdd`, `password`, `type`, `accLvl`) VALUES
-(1, 'John', 'Wick', 'JohnWick', '', 0, 'john@gmail.com', 'jw123', 'admin', 0),
-(2, 'Joaquin', 'Cortez', 'jaycee', '', 0, 'jc@gmail.com', 'jc123', 'Employee', 0),
-(5, 'Roselia', 'Cruz', 'Rose', '', 0, 'rose@email.com', 'rose123', 'Manager', 0),
-(6, 'Jasmine', 'Mendoza', 'Jess', '', 0, 'jess@email.com', 'jess123', 'Employee', 0);
+INSERT INTO `appointment` (`id`, `customer`, `employee`, `service_id`, `date`, `time`, `status`) VALUES
+(1, 'username', 'apolinariom', 1, '2019-03-21', '03:00:00', 'Pending'),
+(2, 'username', 'apolinariom', 1, '2019-03-21', '03:00:00', 'Accepted');
 
 -- --------------------------------------------------------
 
@@ -103,9 +53,8 @@ INSERT INTO `employee` (`employeeId`, `firstName`, `lastName`, `userName`, `addr
 --
 
 CREATE TABLE `review` (
-  `reviewId` int(11) NOT NULL,
-  `custId` int(11) NOT NULL,
-  `firstName` varchar(20) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `customer` varchar(50) NOT NULL COMMENT 'customer''s username',
   `rating` varchar(2) DEFAULT NULL,
   `feedback` varchar(80) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -117,7 +66,7 @@ CREATE TABLE `review` (
 --
 
 CREATE TABLE `schedule` (
-  `schedId` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -129,44 +78,101 @@ CREATE TABLE `schedule` (
 --
 
 CREATE TABLE `service` (
-  `serviceId` int(11) NOT NULL,
-  `serviceName` varchar(19) NOT NULL,
-  `serviceType` varchar(20) NOT NULL,
-  `serviceDescription` varchar(120) NOT NULL,
-  `price` int(4) NOT NULL,
-  `averageTime` varchar(20) NOT NULL
+  `id` int(11) NOT NULL,
+  `service_name` varchar(19) DEFAULT NULL,
+  `service_type` int(11) DEFAULT NULL,
+  `service_description` varchar(120) DEFAULT NULL,
+  `price` varchar(25) DEFAULT NULL,
+  `hs_price` varchar(25) NOT NULL,
+  `average_time` varchar(25) NOT NULL,
+  `archive` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `service`
 --
 
-INSERT INTO `service` (`serviceId`, `serviceName`, `serviceType`, `serviceDescription`, `price`, `averageTime`) VALUES
-(1, 'Hair Service', '', 'Hair Service Description				', 100, '10-15 Minutes'),
-(2, 'Brow Service', '', 'Brow Service Description', 100, '5-10 Minutes'),
-(3, 'Nail Service', '', 'Nail Service Description Here	', 100, '8-10 Minutes'),
-(4, 'Waxing Service', '', 'Waxing Service Description', 100, '10-15 Minutes'),
-(5, 'Event Service', '', 'Event Service Description		', 5000, '1-3 Hours');
+INSERT INTO `service` (`id`, `service_name`, `service_type`, `service_description`, `price`, `hs_price`, `average_time`, `archive`) VALUES
+(1, 'Threadings', 2, 'Service description.', '150', '', '15', 0),
+(4, 'Test Service', 1, 'Service description.', '123', '', '1', 1),
+(5, 'Test Service', 1, 'Service description.', '123', '', '1', 1),
+(6, 'Test Service', 1, 'Service description.', '123', '', '1', 1),
+(7, 'Test Service', 1, 'Service description.', '123', '', '1', 1),
+(8, 'Test Service', 1, 'Service description.', '123', '', '1', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `type`
+-- Table structure for table `service_type`
 --
 
-CREATE TABLE `type` (
-  `typeId` int(11) NOT NULL,
-  `type` varchar(10) NOT NULL
+CREATE TABLE `service_type` (
+  `id` int(11) NOT NULL,
+  `service_type` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `type`
+-- Dumping data for table `service_type`
 --
 
-INSERT INTO `type` (`typeId`, `type`) VALUES
-(1, 'admin'),
-(2, 'manager'),
-(3, 'employee');
+INSERT INTO `service_type` (`id`, `service_type`) VALUES
+(1, 'Hair Service'),
+(2, 'Brow Service'),
+(3, 'Nail Service'),
+(4, 'Waxing Service'),
+(5, 'Event Service');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_account`
+--
+
+CREATE TABLE `user_account` (
+  `username` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `type` varchar(25) NOT NULL,
+  `archive` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_account`
+--
+
+INSERT INTO `user_account` (`username`, `email`, `password`, `type`, `archive`) VALUES
+('andresb', 'andresb@email.com', '$2y$10$boYBdNWZXgeGmT74eSigieJVMBeGCDI7S6tQrnyHVHVeAataazzvS', 'Admin', 0),
+('apolinariom', 'apolinariom@email.com', '$2y$10$boYBdNWZXgeGmT74eSigieJVMBeGCDI7S6tQrnyHVHVeAataazzvS', 'Employee', 0),
+('joserizal', 'joser@email.com', '$2y$10$boYBdNWZXgeGmT74eSigieJVMBeGCDI7S6tQrnyHVHVeAataazzvS', 'Customer', 0),
+('kendallb', 'kendall@email.com', '$2y$10$boYBdNWZXgeGmT74eSigieJVMBeGCDI7S6tQrnyHVHVeAataazzvS', 'Manager', 0),
+('test', 'test@mail.com', '$2y$10$e9Dc2o3fzutQmygaiXwUJO8IewTQkgy0hkproRcrv4ZLtThg/BAJS', 'Customer', 0),
+('username', 'lysle@mail.com', '$2y$10$/RcaF/tNQNxJ5CLnPquvSOWyuPaub..mWCZt0H8aG.rGJue0aWubq', 'Customer', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `username` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `address` varchar(250) DEFAULT NULL,
+  `contact_no` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`username`, `first_name`, `last_name`, `address`, `contact_no`) VALUES
+('andresb', 'Andres', 'Bonifacio', 'Sample Address', '09123456789'),
+('apolinariom', 'Apolinario', 'Mabini', 'Address', '09123456789'),
+('joserizal', 'Jose', 'Rizal', 'Address', '09123456789'),
+('kendallb', 'Kendall Dave', 'Balas', 'Address', '09123456789'),
+('username', 'Lysle', 'B', '123 Address, City, Province', '09123456789'),
+('test', 'Test', 'Test', '123 Address, City, Province', '09123456789');
 
 --
 -- Indexes for dumped tables
@@ -176,82 +182,78 @@ INSERT INTO `type` (`typeId`, `type`) VALUES
 -- Indexes for table `appointment`
 --
 ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`appId`),
-  ADD KEY `custId` (`custId`),
-  ADD KEY `employeeId` (`employeeId`),
-  ADD KEY `serviceId` (`serviceId`);
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`custId`),
-  ADD UNIQUE KEY `firstName` (`firstName`,`lastName`,`userName`,`emailAdd`);
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`employeeId`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `custId` (`customer`),
+  ADD KEY `employeeId` (`employee`),
+  ADD KEY `serviceId` (`service_id`);
 
 --
 -- Indexes for table `review`
 --
 ALTER TABLE `review`
-  ADD PRIMARY KEY (`reviewId`),
-  ADD KEY `userId` (`custId`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`customer`);
 
 --
 -- Indexes for table `schedule`
 --
 ALTER TABLE `schedule`
-  ADD PRIMARY KEY (`schedId`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `service`
 --
 ALTER TABLE `service`
-  ADD PRIMARY KEY (`serviceId`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `service_type` (`service_type`);
 
 --
--- Indexes for table `type`
+-- Indexes for table `service_type`
 --
-ALTER TABLE `type`
-  ADD PRIMARY KEY (`typeId`);
+ALTER TABLE `service_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_account`
+--
+ALTER TABLE `user_account`
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email_address` (`email`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `username_2` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `customer`
+-- AUTO_INCREMENT for table `appointment`
 --
-ALTER TABLE `customer`
-  MODIFY `custId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `employee`
---
-ALTER TABLE `employee`
-  MODIFY `employeeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `schedule`
 --
 ALTER TABLE `schedule`
-  MODIFY `schedId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `service`
 --
 ALTER TABLE `service`
-  MODIFY `serviceId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `type`
+-- AUTO_INCREMENT for table `service_type`
 --
-ALTER TABLE `type`
-  MODIFY `typeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `service_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -261,15 +263,27 @@ ALTER TABLE `type`
 -- Constraints for table `appointment`
 --
 ALTER TABLE `appointment`
-  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`custId`) REFERENCES `customer` (`custId`),
-  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`employeeId`) REFERENCES `employee` (`employeeId`),
-  ADD CONSTRAINT `appointment_ibfk_3` FOREIGN KEY (`serviceId`) REFERENCES `service` (`serviceId`);
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`),
+  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`customer`) REFERENCES `user_info` (`username`),
+  ADD CONSTRAINT `appointment_ibfk_3` FOREIGN KEY (`employee`) REFERENCES `user_info` (`username`);
 
 --
 -- Constraints for table `review`
 --
 ALTER TABLE `review`
-  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`custId`) REFERENCES `customer` (`custId`);
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`customer`) REFERENCES `user_account` (`username`);
+
+--
+-- Constraints for table `service`
+--
+ALTER TABLE `service`
+  ADD CONSTRAINT `service_ibfk_1` FOREIGN KEY (`service_type`) REFERENCES `service_type` (`id`);
+
+--
+-- Constraints for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD CONSTRAINT `user_info_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user_account` (`username`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
